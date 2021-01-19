7b9346cdd18f624ef9e763eb7bceb27884b9ec3d
arma7x <arma7x@live.com>
2021-01-20 02:24:27 +0800

    update

--- client/sw.js
+++ client/sw.js
@@ -1,4 +1,7 @@
+console.log('[SW] Push worker started')
+
 self.addEventListener('push', function(event) {
+  console.log('[SW] push')
   const data = event.data.json()
   self.registration.showNotification(data.title || 'Default Title', {
     body: data.body || 'Default notification body',
@@ -6,14 +9,14 @@ self.addEventListener('push', function(event) {
 });
 
 self.addEventListener('pushsubscriptionchange', function(event) {
-  //console.dir('pushsubscriptionchange', event)
+  console.log('[SW] pushsubscriptionchange')
   //self.registration.showNotification('pushsubscriptionchange', {
   //  body: 'This is pushsubscriptionchange',
   //});
 });
 
 self.addEventListener('install', function(event) {
-  //console.dir('install', event)
+  console.log('[SW] install')
   self.skipWaiting()
   //self.registration.showNotification('install', {
     //body: 'This is install',
@@ -22,9 +25,29 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('activate', function(event) {
-  //self.skipWaiting()
-  //console.dir('activate', event)
+  self.skipWaiting()
+  console.log('[SW] activate')
   //self.registration.showNotification('activate', {
   //  body: 'This is activate',
   //});
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[SW] notificationclick');
+  event.notification.close();
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow) {
+      return clients.openWindow('/');
+    }
+    if (clients.openApp) {
+      return clients.openApp();
+    }
+  }));
 });

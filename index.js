8638982883a5434af0973b37667a6c8b30f6aa0e
arma7x <arma7x@live.com>
2021-05-25 17:47:57 +0800

    update

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: 'variables.env' })
 
-const express = require('express');
-const webPush = require('web-push');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require('express')
+const webPush = require('web-push')
+const bodyParser = require('body-parser')
+const path = require('path')
 const cors = require('cors')
 
-const app = express();
+const app = express()
 
 app.use(cors())
 
 app.use(cors())
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client')))
 
-const subject = 'mailto:test@example.com';
-const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
-const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+const subject = 'mailto:test@example.com'
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY
 
-webPush.setVapidDetails(subject, publicVapidKey, privateVapidKey);
+webPush.setVapidDetails(subject, publicVapidKey, privateVapidKey)
 
 app.post('/subscribe', (req, res) => {
 
   const body = req.body
 
+  const encoding = req.body.encoding ? req.body.encoding : 'aesgcm'
+
   const payload = req.body.payload ? JSON.stringify(req.body.payload) : JSON.stringify({
     title: 'Push Notification',
     body: 'Push notifications with Service Workers',
-  });
+  })
 
   setTimeout(() => {
-    webPush.sendNotification(body.subscription, payload, {contentEncoding: 'aesgcm'})
+    webPush.sendNotification(body.subscription, payload, {contentEncoding: encoding})
     .then((success) => {
-      console.log(success);
+      console.log(success)
       res.status(success.statusCode).json(success)
     })
     .catch((error) => {
       console.log('Subscription', body.subscription)
-      console.log('RequestDetails', webPush.generateRequestDetails(body.subscription, payload, {contentEncoding: 'aesgcm'}));
-      console.log('Error', error);
+      console.log('RequestDetails', webPush.generateRequestDetails(body.subscription, payload, {contentEncoding: 'aesgcm'}))
+      console.log('Error', error)
       res.status(error.statusCode).json(error)
-    });
-  }, 3000);
-});
+    })
+  }, 3000)
+})
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000)
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+  console.log(`Express running → PORT ${server.address().port}`)
+})
